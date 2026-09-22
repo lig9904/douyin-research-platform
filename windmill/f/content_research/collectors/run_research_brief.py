59d8b5b2034e5ab9458020b94bb62733250ed749
LG-MAC <lg-mac@LG-MACdeMacBook-Pro.local>
2026-09-22 14:40:31 +0800

    build(windmill): pin recent keyword brief logic

--- windmill/f/content_research/collectors/run_research_brief.py
+++ windmill/f/content_research/collectors/run_research_brief.py
@@ -1,7 +1,7 @@
 # /// script
 # requires-python = "==3.13.*"
 # dependencies = [
-#   "douyin-research-platform @ git+https://github.com/lig9904/douyin-research-platform@a46b12647ebeb4a133536bf97c8578161c89bed7",
+#   "douyin-research-platform @ git+https://github.com/lig9904/douyin-research-platform@159c06d0b97e52b6da6e122b51f0811a4a43ab16",
 #   "psycopg[binary]==3.3.6",
 #   "wmill==1.815.0",
 # ]
